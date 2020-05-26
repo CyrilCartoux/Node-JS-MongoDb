6d9a6e4b94a352dbc5217af896e20c9df2141543
Cyril Cartoux <6ssou@protonmail.com>
2020-05-26 21:10:53 +0200

    gitignore

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require("./models/order");
-const OrderItem = require("./models/order-items")
 
 const app = express();
 
@@ -24,49 +17,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findByPk(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
-// RELATIONS : 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-Order.belongsToMany(Product, { through: OrderItem });
-User.hasMany(Order);
-Order.belongsTo(User)
-
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then(result => {
-    return User.findByPk(1);
-  })
-  .then(user => {
-    if (!user) {
-      return User.create({ name: '6ssou', email: 'satoshi@gmx.com' });
-    }
-    return user;
-  })
-  .then(user => {
-    return user.createCart();
-  })
-  .then(cart => {
-    app.listen(3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
--- util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('node_complete', 'root', '', {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: '3308'
-});
-
-module.exports = sequelize;
