1139ff533bdc6c0cbd8d1efcf4220da8703e78f6
Cyril Cartoux <6ssou@protonmail.com>
2020-05-27 18:21:04 +0200

    correction

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const User = require("./models/user")
 const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
 const app = express();
 
 app.set('view engine', 'ejs');
