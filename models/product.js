e0572b298347a2b50c44532cbd611fdedd3c0394
Cyril Cartoux <6ssou@protonmail.com>
2020-05-27 12:35:32 +0200

    edit product by id

--- models/product.js
+++ models/product.js
@@ -2,52 +2,63 @@ const mongodb = require("mongodb")
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products').insertOne(this)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(err => {
-      console.log(err)
-    });
+    let dbOperation;
+    if (this._id) {
+      // update the product
+      dbOperation = db.collection("products").updateOne(
+        { _id: new mongodb.ObjectId(this._id) }, {$set: this}
+      )
+    } else {
+      // insert it
+      dbOperation = db.collection('products').insertOne(this)
+    }
+    return dbOperation
+      .then(result => {
+        console.log(result)
+      })
+      .catch(err => {
+        console.log(err)
+      });
   }
 
   static fetchAll() {
     const db = getDb();
     // find returns a cursor
     return db.collection('products')
-    .find()
-    .toArray()
-    .then(products => {
-      return products;
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .find()
+      .toArray()
+      .then(products => {
+        return products;
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   static findById(prodId) {
     const db = getDb();
     return db.collection('products')
-    // because id are stored differently
-    .find({_id: mongodb.ObjectID(prodId)})
-    // next to get the last document returned by find()
-    .next()
-    .then(product => {
-      console.log(product);
-      return product;
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      // because id are stored differently
+      .find({ _id: mongodb.ObjectID(prodId) })
+      // next to get the last document returned by find()
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 
