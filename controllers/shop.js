f166e2d06336cde6e9359371eb632d0d38b483e4
Cyril Cartoux <6ssou@protonmail.com>
2020-05-27 11:49:48 +0200

    get product by id

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,18 +16,19 @@ exports.getProducts = (req, res, next) => {
 };
 
 // get one product
-// exports.getProduct = (req, res, next) => {
-//   const prodId = req.params.productId;
-//   Product.findByPk(prodId)
-//     .then(product => {
-//       res.render('shop/product-detail', {
-//         product: product,
-//         pageTitle: product.title,
-//         path: '/products'
-//       });
-//     })
-//     .catch(err => console.log(err));
-// };
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(product => {
+      console.log(product)
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
 
 // main page : load products
 exports.getIndex = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require("mongodb")
 const getDb = require("../util/database").getDb;
 
 class Product {
@@ -26,8 +27,23 @@ class Product {
     .find()
     .toArray()
     .then(products => {
-      console.log(products)
       return products;
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products')
+    // because id are stored differently
+    .find({_id: mongodb.ObjectID(prodId)})
+    // next to get the last document returned by find()
+    .next()
+    .then(product => {
+      console.log(product);
+      return product;
     })
     .catch(err => {
       console.log(err)
