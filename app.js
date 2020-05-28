5a624a1861d6baa2108640166445d4f1931f207d
Cyril Cartoux <6ssou@protonmail.com>
2020-05-28 09:40:55 +0200

    add to cart is working (partly)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const User = require("./models/user")
 const mongoConnect = require("./util/database").mongoConnect;
+const mongodb = require('mongodb')
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -20,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("5ece8cb7a00f1de49b918cc2")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, mongodb.ObjectId(user._id));
+      console.log(req.user)
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -2,24 +2,41 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.username = username;
-    this.email = email
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
   }
 
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+
+    // no products in the cart 
+    const updatedCart = { items: [{ ...product, quantity: 1 }] }
+    const db = getDb();
+    console.log(this._id)
+    console.log(mongodb.ObjectId(this._id))
+    return db.collection("users").updateOne(
+      { _id: mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } })
+  }
+
   static findById(userId) {
     const db = getDb()
-    return db.collection("users").find({_id: new mongodb.ObjectId(userId)})
-    .next()
-    .then((user) => {
-      console.log(user)
-      return user
-    })
-    .catch(err => {console.log(err)})
+    return db.collection("users").find({ _id: new mongodb.ObjectId(userId) })
+      .next()
+      .then((user) => {
+        console.log(user)
+        return user
+      })
+      .catch(err => { console.log(err) })
   }
 }
 
